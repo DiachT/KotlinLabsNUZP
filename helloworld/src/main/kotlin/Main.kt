50126febe20e409b205381b153ca3474b942a322
Andrey <127662817+akutnyak@users.noreply.github.com>
2023-03-18 23:55:57 +0200

    Лабораторна_Робота_№2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -7,23 +7,20 @@ fun seed(): String = "akutnyak"
 
 fun labNumber() : Int = BuildConfig.LAB_NUMBER
 
-fun iCalculate(): Double {                      //iCaltulate() = tan(min(abs(x0),abs(x1),abs(x2)
-    val x0 : Int = 84; val x1 : Int = -17; val x2 : Int = -123;                     //Аргументи функції
+fun iCalculate(x0:Int, x1:Int, x2:Int): Double {                      //iCaltulate() = tan(min(abs(x0),abs(x1),abs(x2)
     val ax0 : Int = abs(x0); val ax1 : Int = abs(x1); val ax2 : Int = abs(x2);      //Модулі Аргументів
     val mX : Int = minOf(ax0,ax1,ax2)
     val iresult = tan(mX.toDouble())            //Тангенс мінімального аргументу
     return iresult
 }
-fun dCalculate(): Double{                       //dCaltulate() = (min(abs(x0),abs(x1),abs(x2)^(1/3)
-    val x0 : Double = 38.94; val x1 : Double = 74.4; val x2 : Double =64.97;                //Аргументи функції
+fun dCalculate(x0:Double, x1:Double, x2:Double): Double{                       //dCaltulate() = (min(abs(x0),abs(x1),abs(x2)^(1/3)
     val ax0 : Double = abs(x0); val ax1 : Double = abs(x1); val ax2 : Double = abs(x2);     //Модулі Аргументів
     val mX : Double = minOf(ax0,ax1,ax2)
     val cbrt : Double = 1.0 / 3.0              //Корінь третього ступеня - число у ступені 1/3
     val dresult = mX.pow(cbrt)                 //Корінь третього ступеня мінімального аргументу
     return dresult
 }
-fun strCalculate() : Int{
-    var x0 :String = "ATGCJ"    //Аргументи функції
+fun strCalculate(x0:String) : Int{
     var x1 = x0
     var matches = 0; var i= 0; var ct = 0
     do{
@@ -37,9 +34,9 @@ fun strCalculate() : Int{
 }
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    println("Результат першої функції: ${iCalculate()}")
-    println("Результат другої функції: ${dCalculate()}")
-    println("Результат третьої функції: ${strCalculate()}")
+    println("Результат першої функції: ${iCalculate(84,-17,-123)}")
+    println("Результат другої функції: ${dCalculate(38.94,74.4,64.97)}")
+    println("Результат третьої функції: ${strCalculate("ATGCJ")}")
     startTestUi(seed(), labNumber())
 }
 
