c4d111793bc3fa5a90a597fd8bcb2880cc966909
Andrey <127662817+akutnyak@users.noreply.github.com>
2023-03-19 12:06:20 +0200

    Лабораторна_Робота_№2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -7,8 +7,14 @@ fun seed(): String = "akutnyak"
 
 fun labNumber() : Int = BuildConfig.LAB_NUMBER
 
-fun iCalculate(): Double = tan(minOf(abs(84),abs(-17),abs(123)).toDouble())
-fun dCalculate(): Double = minOf(abs(38.94),abs(74.4),abs(64.97)).pow(1.0/3.0)
+fun minInt(x0: Int, x1: Int, x2: Int): Int {
+    return minOf(x0, x1, x2)
+}
+fun minDouble(x0: Double, x1: Double, x2: Double): Double {
+    return minOf(x0, x1, x2)
+}
+fun iCalculate(): Double = tan(minInt(x0 = abs(84),x1 = abs(-17),x2 = abs(123)).toDouble())
+fun dCalculate(): Double = minDouble(x0 = abs(38.94),x1 = abs(74.4),x2 = abs(64.97)).pow(1.0/3.0)
 fun compareEvenChars(string1: String, string2: String): Int {
     var mismatches = 0
     if (string1.length != string2.length) throw Exception("String must be equal")
@@ -17,8 +23,8 @@ fun compareEvenChars(string1: String, string2: String): Int {
             val char1 = string1[i]
             val char2 = string2[i]
             if (char1 != char2) {
-                if(string1[i] != 'A' && string1[i] != 'C') mismatches++
-                if (string1[i] != 'C' && string1[i] != 'T') mismatches += 2
+                if(string1[i] == 'A' && string1[i] == 'C') mismatches++
+                if (string1[i] == 'C' && string1[i] == 'T') mismatches += 2
             }
         }
     }
