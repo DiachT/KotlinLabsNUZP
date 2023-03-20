71bb617e1bb68c9bd271f066d23f8ee6e59219d6
Nikarionec <nikita2015as@gmail.com>
2023-03-21 01:32:18 +0200

    Лабораторна робота №2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,22 +1,40 @@
+import com.diacht.ktest.compose.startTestUi
+import com.diacht.ktest.library.BuildConfig
+import kotlin.math.abs
+import kotlin.math.ln
+import kotlin.math.max
+import kotlin.math.tan
+
 fun seed(): String = "Nikarionec"
 
-fun labNumber() : Int = 1
+
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
+
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    var age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-
-    val catName: String = "Мурзик \uD83D\uDC08"
-    var weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-
-    var name = "Рудий"
-    name += " \uD83D\uDC06"
-    age = 6
-    weight = 8.2f
-    println("Кошеня №3 - $name віком $age років, та вагою $weight кг")
+    startTestUi(seed(), labNumber())
     }
+fun iCalculate(x0: Int = 41, x1: Int = -125, x2: Int = 49, x3: Int = 10, x4: Int = 67) {
+    val max = maxOf(x0, x1, x2, x3, x4)
+    var result1 = ln(max.toDouble())
+    println("Результат = $result1")
+}
+fun dCalculate(x0: Double = 65.72, x1: Double = -11.22, x2: Double = -102.12, x3: Double =-12.96, x4: Double = -47.38) {
+
+    val arg = abs(x0) + abs(x1) + abs(x2) + abs(x3) + abs(x4)
+    var result2 = tan(arg)
+    println("Результат = $result2")
+}
+
+fun strCalculate(x0: String = "A G T C J", x1: String = "G T C J A"): Int {
+    var count = 0
+    for (i in x0.indices) {
+        if ((x0[i] == 'T' || x0[i] == 'C') && (x1[i] == 'T' || x1[i] == 'C') && x0[i] != x1[i]) {
+            count++
+        }
+    }
+    return count
+    println(count)
+}
