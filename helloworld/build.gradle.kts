f33174b1ab4ebbc322a454dbcbb9c2d531fb1f83
AntonNikitiuk1 <tonioni.on.q1@gmail.com>
2023-04-02 13:18:34 +0300

    Лабораторна робота 2

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -1,7 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.10"
+    kotlin("jvm") version "1.8.0"
+    id("org.jetbrains.compose") version "1.3.0"
     application
 }
 
@@ -10,18 +11,22 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("file://${rootDir}/.m2repo/")
 }
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("com.diacht.ktest:library:1.0.6")
+    implementation(compose.desktop.currentOs)
 }
 
 tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,17 +1,39 @@
+import com.diacht.ktest.compose.startTestUi
+import com.diacht.ktest.library.BuildConfig
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.tanh
+
 fun seed(): String = "AntonNikitiuk1"
 
-fun labNumber() : Int = 1
+fun labNumber() : Int = 2
 
+fun iCalculate(x0: Int = -79, x1: Int = -54, x2: Int = 117, x3: Int = -7): Double {
+    return tanh(x0.toDouble() + x1 + x2 + x3)
+}
+
+fun dCalculate(x0: Double = 1.12, x1: Double = 1.15, x2: Double = -7.79, x3: Double = 31.0, x4: Double = 33.6): Double {
+    return cos(min(min(min(min(x0, x1), x2), x3), x4))
+}
+
+fun strCalculate(x0: String, x1: String): Int {
+    var count = 0
+    if (x0.length % 2 == 0 && x1.length % 2 == 0) {
+        for ((e, x) in x0.withIndex()) {
+            if ((x == 'T' || x == 'C') && x == x1.get(e)) {
+                count += 1
+            }
+            if (x == 'C' && x1.get(e) == 'G') {
+                count += 2
+            }
+        }
+    }
+    return count
+}
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    val catName1: String = "Васько \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $catName1 віком $age років")
-    val catName2: String = "Мурзик \uD83D\uDC08"
-    val weight2: Float = 3.5f
-    println("Кошеня №2 - $catName2 з вагою $weight2 кг")
-    val catName3: String = "Рудий \uD83D\uDC06"
-    val weight1: Float = 8.2f
-    val ageCat3 = 6
-    println("Кошеня №3 - $catName3 віком $ageCat3 років з вагою $weight1 кг")
+    iCalculate()
+    dCalculate()
+    strCalculate("ACCGTC","ACCGTG")
+    startTestUi(seed(), labNumber())
 }
