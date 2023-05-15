71015c60cb1aa879bb1bb03dfa37694ec6102b49
Nikarionec <nikita2015as@gmail.com>
2023-05-15 22:03:27 +0300

    Лабораторна робота №4 варіант #13

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -12,26 +12,31 @@ fun seed(): String = "Nikarionec"
 
 fun labNumber() : Int = BuildConfig.LAB_NUMBER
 
- fun main(args: Array<String>) {
+fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
+    val strList = listOf("1", "2", "3", "4", "5")
 
-     val strList = listOf("1", "2", "3", "4", "5") // приклад аргументів
-     val result = runBlocking { serverDataCalculate(strList) }
-     println(result) // виведе результат розрахунку на екран
-
-    startTestUi(seed(), labNumber())
+    runBlocking {
+        val result = calculateServerDataAsync(strList)
+        println(result)
+        startTestUi(seed(), labNumber())
     }
+}
 
-
-suspend fun serverDataCalculate(strList: List<String>): Double = coroutineScope {
+suspend fun calculateServerDataAsync(strList: List<String>): Double = coroutineScope {
     val baseUrl = "http://diacht.2vsoft.com/api/send-number"
-    var sum = 0
-    for (i in strList.indices) {
-        val url = "$baseUrl?num=${strList[i]}"
-        val result = URL(url).readText().toInt()
-        sum += result
+    val deferredResults = strList.map { num ->
+        async { performServerRequest(baseUrl, num) }
     }
+    val results = deferredResults.awaitAll()
+    val sum = results.sum()
     val absoluteSum = sum.absoluteValue
     ln(absoluteSum.toDouble())
+}
+
+suspend fun performServerRequest(baseUrl: String, num: String): Int = withContext(Dispatchers.IO) {
+    val url = "$baseUrl?num=$num"
+    val result = URL(url).readText().toInt()
+    result
 }
