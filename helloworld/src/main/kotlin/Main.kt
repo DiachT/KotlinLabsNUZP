0b689755a743ccfde7bad3aaa8c900cc78e42875
Nikarionec <nikita2015as@gmail.com>
2023-05-17 12:39:36 +0300

    Лабораторна робота №4 варіант #14

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -2,8 +2,9 @@ import com.diacht.ktest.compose.startTestUi
 import com.diacht.ktest.library.BuildConfig
 import kotlinx.coroutines.*
 import java.net.URL
-import kotlin.math.absoluteValue
 import kotlin.math.ln
+import kotlin.math.abs
+
 
 
 
@@ -25,14 +26,13 @@ fun main(args: Array<String>) {
 }
 
 suspend fun serverDataCalculate(strList: List<String>): Double = coroutineScope {
-    val baseUrl = "http://diacht.2vsoft.com/api/send-number"
+    val baseUrl = "http://diacht.2vsoft.com/api/send-number?message="
     val deferredResults = strList.map { num ->
-        async { performServerRequest(baseUrl, num) }
+        async { performServerRequest(baseUrl + num, num) }
     }
     val results = deferredResults.awaitAll()
-    val sum = results.sum()
-    val absoluteSum = sum.absoluteValue
-    ln(absoluteSum.toDouble())
+    val sum = results.sumOf { abs(it).toDouble() }
+    ln(sum)
 }
 
 suspend fun performServerRequest(baseUrl: String, num: String): Int = withContext(Dispatchers.IO) {
