1ba71fc0b450cc2a10ec6bfc5500abcff6e422ed
Nikarionec <sergeevdanilka6@gmail.com>
2023-05-31 22:13:57 +0300

    Лабораторна робота №2 варіант #12

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -1,16 +1,21 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.compose") version Versions.compose
     application
 }
 
-group = "me.nikarionec"
+group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("file://${rootDir}/.m2repo/")
 }
 
 dependencies {
+    implementation(Versions.library)
+    implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
 }
 
@@ -19,7 +24,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(Versions.jvmLevel)
 }
 
 application {
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,22 +1,54 @@
+import com.diacht.ktest.compose.startTestUi
+import org.example.helloworld.BuildConfig
+import kotlin.math.tan
+import kotlin.math.sqrt
+
 fun seed(): String = "PingvinSt"
 
-fun labNumber() : Int = 1
+fun labNumber(): Int = BuildConfig.LAB_NUMBER
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
+    val result1 = iCalculate()
+    println(result1)
+    val result2 = dCalculate()
+    println(result2)
+    val x0 = "ATGCT"
+    val x1 = "CTCJT"
+    val result3 = strCalculate(x0, x1)
+    println(result3)
 
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
 
-    val catName2: String = "Рудий \uD83D\uDC06"
-    val age2: Int = 6
-    val weight2: Float = 8.2f
-    println("Кошеня №3 - $catName2 віком $age2 років та має вагу $weight2 кг.")
 
+    startTestUi(seed(), labNumber())
+}
+
+fun dCalculate(
+    x0: Double = -52.48,
+    x1: Double = 15.12,
+    x2: Double = 0.66,
+    x3: Double = 0.84
+): Double {
+    return sqrt(x0 * x1 * x2 * x3)
+}
+
+fun iCalculate(x0: Int = -18, x1: Int = 17, x2: Int = -53): Double {
+    return tan(x0 * x1 * x2.toDouble())
+}
+
+fun strCalculate(x0: String, x1: String): Int {
+    require(x0.length == x1.length) { "x0 and x1 must have the same length" }
+
+    var differenceCount = 0
+    for (i in x0.indices) {
+        val charX0 = x0[i]
+        val charX1 = x1[i]
+
+        if ((charX0 == 'T' || charX0 == 'C') && charX1 != 'T' && charX1 != 'C') {
+            differenceCount++
+        }
+    }
+
+    return differenceCount
 }
