2a8251ff641f593f6f2c77b5869bf72a215d024f
svetlanakovalevskaya <lana.worker22@gmail.com>
2023-06-03 20:03:29 +0300

    fun iCalculate(), fun dCalculate()

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import com.diacht.ktest.compose.startTestUi
+import kotlin.math.*
 
 fun seed(): String = "lanakovalevska"
 
@@ -6,6 +7,10 @@ fun labNumber() : Int = 2
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-
+    println(iCalculate())
+    println(dCalculate())
     startTestUi(seed(), labNumber())
 }
+fun iCalculate(x0: Int = -42, x1: Int = -110, x2: Int = -62) : Double = tanh(x0.toDouble()*x1.toDouble()*x2.toDouble())
+fun dCalculate(x0: Double = -18.72, x1: Double = 1.92, x2: Double = 8.91) : Double = sin(x0*x1*x2)
+
