7dc08364e42d7eaab8c0f40384cc685d23941895
Nikarionec <sergeevdanilka6@gmail.com>
2023-06-04 17:27:02 +0300

    Лабораторна робота №2 варіант #12

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,22 +1,64 @@
+import com.diacht.ktest.compose.startTestUi
+import org.example.helloworld.BuildConfig
+import kotlin.math.tan
+import kotlin.math.sqrt
+import kotlin.math.abs
+
 fun seed(): String = "PingvinSt"
 
-fun labNumber() : Int = 1
+
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
+    val iCalcResult = iCalculate()
+    println("Результат iCalculate: $iCalcResult")
 
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
+    val dCalcResult = dCalculate()
+    println("Результат dCalculate: $dCalcResult")
 
-    val catName2: String = "Рудий \uD83D\uDC06"
-    val age2: Int = 6
-    val weight2: Float = 8.2f
-    println("Кошеня №3 - $catName2 віком $age2 років та має вагу $weight2 кг.")
+    val strCalcResult = strCalculate()
+    println("Результат strCalculate: $strCalcResult")
 
+    startTestUi(seed(), labNumber())
+    }
+
+fun iCalculate(x0: Int = -61, x1: Int = -42, x2: Int = 32, x3: Int = 60): Double {
+    val sum = abs(x0) + abs(x1) + abs(x2) + abs(x3)
+    return sqrt(sum.toDouble())
+}
+
+fun dCalculate(
+    x0: Double = -23.4,
+    x1: Double = -9.3,
+    x2: Double = -33.6,
+    x3: Double = -31.39,
+    x4: Double = -41.8
+): Double {
+    val sum = abs(x0) + abs(x1) + abs(x2) + abs(x3) + abs(x4)
+    return tan(sum)
+}
+
+fun strCalculate(
+    x0: String = "ATGCJ",
+    x1: String = "ATCJG"
+): Int {
+    require(x0.length == x1.length) { "Рядки повинні мати однакову довжину" }
+
+    var difference = 0
+    for (i in x0.indices) {
+        val charX0 = x0[i]
+        val charX1 = x1[i]
+
+        if (charX0 != charX1) {
+            if (i >= x0.length / 2) {
+                difference += 2
+            } else {
+                difference++
+            }
+        }
+    }
+
+    return difference
 }
