b9eff17e42afd639bd04f61d6eb93729b07317ac
svetlanakovalevskaya <lana.worker22@gmail.com>
2023-06-05 21:12:14 +0300

    Виправлене визначення довжини рядка

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -14,9 +14,9 @@ fun main(args: Array<String>) {
 }
 fun iCalculate(x0: Int = -42, x1: Int = -110, x2: Int = -62) : Double = tanh(x0.toDouble()*x1.toDouble()*x2.toDouble())
 fun dCalculate(x0: Double = -18.72, x1: Double = 1.92, x2: Double = 8.91) : Double = sin(x0*x1*x2)
-fun strCalculate(x0: String = "AGJATC", x1: String = "ACATTG") : Int{
+fun strCalculate(x0: String = "AGJATC", x1: String = "ACATTG") : Int {
     var res: Int = 0
-    var index: Int = 5
+    var index: Int = x0.length-1
     while (index >= 0) {
         if (x0[index] != 'A' && x0[index] != 'C') {
             res += 0
