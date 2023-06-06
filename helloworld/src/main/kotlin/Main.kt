96d4f6f95508a87990229baebe7f24c1c2600239
svetlanakovalevskaya <lana.worker22@gmail.com>
2023-06-06 13:25:11 +0300

    Робоче завдання №3

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -14,22 +14,18 @@ fun main(args: Array<String>) {
 }
 fun iCalculate(x0: Int = -42, x1: Int = -110, x2: Int = -62) : Double = tanh(x0.toDouble()*x1.toDouble()*x2.toDouble())
 fun dCalculate(x0: Double = -18.72, x1: Double = 1.92, x2: Double = 8.91) : Double = sin(x0*x1*x2)
-fun strCalculate(x0: String = "AGJATC", x1: String = "ACATTG") : Int {
+fun strCalculate(x0: String = "GACCAJJAJTCJCJATCAJGTTTGTGCATCTACJJTJCGCJCAAJGAJTJTTGGATCJGAJTGJATTGCCTGCJJAGGAGCGAGGCJATTGGTAJGACCJGAAJGCGJTTGJTTTJGTJGJCTJTCTGGCGCGJGJTAGGACCCGGAAJGGJATAGAGTTJAJATCACGJAJAJATCJTATTCJCAGTTCAAATTJGJJJJTJCJCGGCACGACGJTJTJTGTCCAAGJTTGJCTGAAJACJGAGGAAJCCATJGCCCACAJTCJJGJCA", x1: String = "GACCAJJAJTCJCJATCAJGTTTGTGTATCTACJJTJCGCJCAAJGAJTJTTGGATGJGAJTGJATTGCCTGCJJAGGAGTCAGGCJATTGGTAAGACCJGAJGGCGJTTJJTTTJGTJGJCTJTCTGGCGCGJGJTAGGACCCGGAAJGGJATAGAGTTJAJATCACGJAJAJATCJTATTCJCAGTTCAAATTJGJJJJTJCJCGGCACATCGJTJTJTGTCCAAGJTTGJCTGAAJACJGAGGAAJCCATJGCCCACAJTCJJGJCA") : Int {
     var res: Int = 0
     var index: Int = x0.length-1
     while (index >= 0) {
-        if (x0[index] != 'A' && x0[index] != 'C') {
-            res += 0
-        }
-        else if (x0[index] == 'A' && x1[index] == 'A') {
-            res +=0
-        }
-        else if (x0[index] == 'C' && x1[index] == 'C') {
-            res += 0
-        }
-        else if (x0[index] == 'С' && x1[index] == 'T' ) {
-            res += 2
-        } else {
+        if (x0[index] != 'A' && x0[index] != 'C') {}
+        else if (x0[index] == 'A' && x1[index] == 'A') {}
+        else if (x0[index] == 'A' && x1[index] != 'A' ) {
+            res +=1
+        } else if (x0[index] == 'C' && x1[index] == 'T' ) {
+            res +=2
+        } else if (x0[index] == 'C' && x1[index] == 'C') {}
+        else {
             res += 1
         }
 
