9c20b7a7aa60994da39c0f3c07bfda43892afc69
yzadorozhnij <zadorozhnijyara@gmail.com>
2023-08-02 19:11:10 +0300

    Лабораторна робота №1 Варіант №19

---
+++ helloworld/src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+fun seed(): String = "yzadorozhnij"
+
+fun labNumber() : Int = 1
+
+fun main(args: Array<String>) {
+    println("Лабораторна робота №${labNumber()} користувача ${seed()}")
+    var kitty = "Васько"
+    kitty += " \uD83D\uDC31"
+    val age = 7
+    println("Кошеня №1 - $kitty віком $age років")
+
+    val catName: String = "Мурзик \uD83D\uDC08"
+    val weight: Float = 3.5f
+    println("Кошеня №2 - $catName з вагою $weight кг")
+
+    var catName2: String = "Рудий \uD83D\uDC06"
+    val age2 = 6
+    val weight2: Float = 8.2f
+    println("Кошеня №3 - $catName2 віком з $age2 та вагою $weight2 кг")
+}
