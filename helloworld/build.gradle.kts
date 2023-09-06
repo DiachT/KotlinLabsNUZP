5ab14b95db04a0219db9a28f1a062dba8d4d28a0
VanUA <vanatom22@gmail.com>
2023-09-06 19:31:44 +0300

    work_lab2

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -1,29 +1,32 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.compose") version Versions.compose
     application
-}
+    }
 
-group = "me.vanat"
+group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("file://${rootDir}/.m2repo/")
+    }
 
 dependencies {
+    implementation(Versions.library)
+    implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
-}
+    }
 
 tasks.test {
     useJUnitPlatform()
-}
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+kotlin {
+    jvmToolchain(Versions.jvmLevel)
+    }
 
 application {
     mainClass.set("MainKt")
-}
+    }
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,21 +1,38 @@
+import com.diacht.ktest.compose.startTestUi
+import me.vanya.helloworld.BuildConfig
+import kotlin.math.*
+
 fun seed(): String = "progvan"
 
-fun labNumber() : Int = 1
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
+
+fun iCalculate(x0:Int = 61, x1:Int = 116, x2:Int = -63, x3:Int = 25): Double {
+    return sin(x0.toDouble() + x1.toDouble() + x2.toDouble() + x3.toDouble())
+}
+
+fun dCalculate(x0:Double = -4.7, x1:Double = -119.78, x2:Double = -11.55, x3:Double = -8.88, x4:Double = -25.22): Double {
+    return cbrt(x0 * x1 * x2 * x3 * x4)
+}
+
+fun strCalculate(x0: String, x1: String) : Int {
+        var result:Int = 0
+
+        for (i in x0.indices) {
+            val charX0 = x0[i]
+            val charX1 = x1[i]
+
+            if ((charX0 == 'T' || charX0 == 'C') && (charX1 != 'C' && charX1 != 'T')) {
+                result += 1
+            } else if ((charX0 == 'T' && charX1 == 'C') || (charX0 == 'C' && charX1 == 'T')) {
+                result += 2
+            } else {
+                result += 0
+            }
+        }
+        return result
+}
 
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-
-    val catName2: String = "Рудий \uD83D\uDC06"
-    val weight2: Float = 8.2f
-    val age2 = 6
-    println("Кошеня №3 - $catName2 віком $age2 років з вагою $weight2 кг")
-}
+    startTestUi(seed(), labNumber())
+    }
