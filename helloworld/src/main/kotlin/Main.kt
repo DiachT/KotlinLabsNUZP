256c42a6b4eda49612a1f6c473bee050d3e1b788
Zaytsev Alexandr <zaytsev.alexandr41@gmail.com>
2023-10-23 14:28:30 +0300

    Update Main.kt

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,39 +1,40 @@
  import com.diacht.ktest.compose.startTestUi
  import me.user.helloworld.BuildConfig
- import kotlin.math.abs
- import kotlin.math.cos
- import kotlin.math.max
- import kotlin.math.sqrt
+ import kotlin.math.*
 
- fun seed(): String = "Alexandr"
+ fun seed(): String = "Niaekk"
 
 
  fun labNumber() : Int = BuildConfig.LAB_NUMBER
- fun  iCalculate(x0:Int=-42,x1:Int=14,x2:Int=-121):Double {
-val a=max(x0,x1)
+ fun  iCalculate(x0:Int=-89,x1:Int=-77,x2:Int=50,x3:Int=-55,x4:Int=87):Double {
+val a=abs(x0)+abs(x1)+abs(x2)+abs(x3)+abs(x4)
   val b: Int =max(a,x2)
-  return sqrt(b.toDouble())
+  return sqrt(a.toDouble())
  }
- fun dCalculate(x0:Double=55.35,x1:Double=-16.72,x2:Double=92.82) : Double{
+ fun dCalculate(x0:Double=25.01,x1:Double=-78.66,x2:Double=42.84,x3:Double=50.22,x4:Double=33.5) : Double{
   val a=max(abs(x0),abs(x1))
-  val b=max(abs(x2),a)
-  return cos(b)
+  val b=max(abs(x2),abs(x3))
+  val c=max(a,b)
+  val d=max(c,abs(x4))
+  return ln(d)
  }
  fun strCalculate(x0:String,x1:String) : Int{
   var a=0
   var y=0
   do {
-   if(x0[y]=='T' || x0[y]=='C'){
-    if ((x0[y] == 'T')  && (x1[y] != 'J') && (x1[y] != 'T'))
-     a++
-    if ((x0[y] == 'C')  && (x1[y] != 'C') )
-     a++
+   if(x0[y]!=x1[y]) {
+    if ((x0[y] == 'C' && x1[y] == 'T') || (x0[y] == 'T' && x1[y] == 'C')) {
+     a = a + 2
+     y = y + 2
+    }
+    else{
+    a++
+    y=y+2}
    }
-   y++
+   else y=y+2
   }while (y!=x0.length)
   return a
  }
-
  fun main(args: Array<String>) {
    println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
