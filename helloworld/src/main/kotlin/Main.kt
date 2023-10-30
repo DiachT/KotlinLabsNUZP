3ca30c625a9cfede6faadc1919a246639ee68f2e
Malnormalulo <malnormalulo1@gmail.com>
2023-10-30 20:03:26 +0200

    lab4

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import com.diacht.ktest.FactoryItf
 import com.diacht.ktest.compose.startTestUi
-import me.malno.helloworld.BuildConfig
+import com.diacht.ktest.library.BuildConfig
+import kotlinx.coroutines.*
+//import me.malno.helloworld.BuildConfig
+import java.net.URL
 
 fun seed(): String = "Malnormalulos-git"
 fun labNumber() : Int = BuildConfig.LAB_NUMBER
@@ -13,7 +16,28 @@ fun getSimulationObject() : FactoryItf {
 	return factory
 }
 
-fun main(args: Array<String>) {
+suspend fun getNumberFromServer(string: String): Int {
+	return withContext(Dispatchers.IO) {
+		val url = URL("http://diacht.2vsoft.com/api/send-number?message=$string")
+		val connection = url.openConnection()
+		connection.connect()
+		val input = connection.getInputStream()
+		val buffer = ByteArray(128)
+		val bytesRead = input.read(buffer)
+		input.close()
+		String(buffer, 0, bytesRead).toInt()
+	}
+}
+
+
+suspend fun serverDataCalculate(messages: List<String>) : Double = coroutineScope {
+	val deferredList = messages.map{  async { getNumberFromServer(it) } }
+	var XsProduct = 1.toLong()
+	deferredList.awaitAll().forEach{ XsProduct *= it }
+	Math.cbrt(XsProduct.toDouble())
+}
+
+fun main(args: Array<String>){
 	println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 	startTestUi(seed(), labNumber())
 }
