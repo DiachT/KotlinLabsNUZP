b7b0b7f78e4fc74ea9a62bb1d03599c1f421ff47
Нікіта Пітюренко <fhnf30.5f@gmail.com>
2023-11-28 09:32:56 +0200

    Лабораторна робота #4 варіант 54

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -4,6 +4,13 @@ import kotlin.math.abs
 import kotlin.math.cos
 import kotlin.math.sin
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.withContext
+import java.net.URL
+import kotlin.math.sin
+
 fun seed(): String = "Nikita-Pityurenko"
 
 fun labNumber() : Int = BuildConfig.LAB_NUMBER
@@ -11,28 +18,27 @@ fun labNumber() : Int = BuildConfig.LAB_NUMBER
 
 
 
-    fun iCalculate(x0: Int = 17, x1: Int = 121, x2: Int = -68, x3: Int = 32, x4: Int = -1):
-            Double {
-        val sum = abs(x0.toDouble()) + abs(x1.toDouble()) + abs(x2.toDouble()) + abs(x3.toDouble()) + abs(x4.toDouble())
-        return cos(sum)
-    }
-    fun dCalculate(x0: Double = 33.64, x1: Double = -40.0, x2: Double = -8.75, x3: Double = -26.19, x4: Double = 40.95): Double {
-        return sin(x0 * x1 * x2 * x3 * x4)
-    }
-    fun strCalculate(x0: String, x1: String): Int {
-        var count = 0
-        for (i in x0.indices) {
-            if (i % 2 == 0) { // проверяем только парные символы
-                if (x0[i] != x1[i]) {
-                    if (!((x0[i] == 'T' && x1[i] == 'J') || (x0[i] == 'J' && x1[i] == 'T'))) {
 
-                        count++
-                    }
-                }
-            }
-        }
-        return count
+
+suspend fun getNumberFromServer(message: String): Int {
+    return withContext(Dispatchers.IO) {
+        val url = URL("http://diacht.2vsoft.com/api/send-number?message=$message")
+        val connection = url.openConnection()
+        connection.connect()
+        val input = connection.getInputStream()
+        val buffer = ByteArray(128)
+        val bytesRead = input.read(buffer)
+        input.close()
+        String(buffer, 0, bytesRead).toInt()
     }
+}
+
+suspend fun serverDataCalculate(strList: List<String>) : Double = coroutineScope {
+    val numbers = strList.map { async { getNumberFromServer(it) } }
+    val product = numbers.fold(1.0) { acc, number -> acc * number.await() }
+    sin(product)
+}
+
 fun main (args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
 
