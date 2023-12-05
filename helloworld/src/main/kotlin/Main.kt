931d00078bb3cc609071d1f5e4509e6189ba6334
ne4eHIOLLIka <152488731+ne4eHIOLLIka@users.noreply.github.com>
2023-12-05 21:59:57 +0200

    лабораторна робота 2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,17 +1,8 @@
-
+import com.diacht.ktest.compose.startTestUi
+import  me.alexander.helloworld.BuildConfig
 fun seed(): String = "ne4eHIOLLIka"
-fun labNumber() : Int = 1
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-    val cat3: String="Рудий \uD83D\uDC06"
-    val cat3age: Int=6
-    val cat3weight: Float=8.2f
-    println("Кошеня №3 - $cat3 віком $cat3age з вагою $cat3weight кг")
+    startTestUi(seed(), labNumber())
 }
