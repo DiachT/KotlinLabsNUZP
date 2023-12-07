7a36fb525372b4a566d8bedc8bd38ca79b2eb0b8
Zilagar <datacasa8@gmail.com>
2023-12-07 20:27:56 +0200

    update 4 lav
    update 4 lav

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,29 +1,19 @@
 
+import com.diacht.ktest.compose.startTestUi
 import kotlinx.coroutines.*
 import org.example.helloworld.BuildConfig
-import java.lang.Math.tanh
 import java.net.URL
+import kotlin.math.tanh
 
 fun seed(): String = "Zilagar"
 
 fun labNumber(): Int = BuildConfig.LAB_NUMBER
-
-fun main() = runBlocking {
-    //startTestUi(seed(), labNumber())
-    val messages = listOf("one", "two", "three", "four","five","six")
-    val res = serverDataCalculate(messages)
-    println("Result: $res")
-}
 
 suspend fun getNumberFromServer(message: String): Int {
     return withContext(Dispatchers.IO) {
         val url = URL("http://diacht.2vsoft.com/api/send-number?message=$message")
         val connection = url.openConnection()
         connection.connect()
-
-
-
-
         val input = connection.getInputStream()
         val buffer = ByteArray(128)
         val bytesRead = input.read(buffer)
@@ -32,16 +22,19 @@ suspend fun getNumberFromServer(message: String): Int {
     }
 }
 
-suspend fun serverDataCalculate(strList: List<String>): Unit = coroutineScope {
+suspend fun serverDataCalculate(strList: List<String>): Double = coroutineScope {
     val deferredList = strList.map { async { getNumberFromServer(it) } }
     val result = deferredList.awaitAll()
     println("Numbers: $result")
 
-
-
     val abs = result.map { Math.abs(it) }
     val maxValue   = abs.max()
-    val res = tanh(maxValue.toDouble())
-    println("$res ")
-    }
+    tanh(maxValue.toDouble())
+}
 
+fun main() = runBlocking {
+    startTestUi(seed(), labNumber())
+    val messages = listOf("one", "two", "three", "four","five","six")
+    val res = serverDataCalculate(messages)
+    println("Result: $res")
+}
