0d0c0d5b511dbb15c63d4513f566d973a7761fab
ne4eHIOLLIka <152488731+ne4eHIOLLIka@users.noreply.github.com>
2024-01-31 15:05:29 +0200

    lab 2 variant 41

--- helloworld/build.gradle.kts
+++ helloworld/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.compose") version Versions.compose
     application
 }
 
@@ -7,10 +8,14 @@ group = "me.alexander"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("file://${rootDir}/.m2repo/")
     mavenCentral()
 }
 
 dependencies {
+    implementation(Versions.library)
+    implementation(compose.desktop.currentOs)
     testImplementation(kotlin("test"))
 }
 
@@ -19,7 +24,7 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+    jvmToolchain(Versions.jvmLevel)
 }
 
 application {
--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,19 +1,36 @@
+import com.diacht.ktest.compose.startTestUi
+import me.alexander.helloworld.BuildConfig
+import kotlin.math.min
+import kotlin.math.sin
+import kotlin.math.cbrt
 fun seed(): String = "ne4eHIOLLIka"
-
-fun labNumber() : Int = 1
+fun labNumber(): Int = BuildConfig.LAB_NUMBER
+fun iCalculate(x0: Int, x1: Int, x2: Int, x3: Int, x4: Int): Double {
+    val a=minOf(x0, x1, x2, x3, x4)
+    return sin(a.toDouble())
+}
+fun dCalculate(x0: Double, x1: Double, x2: Double): Double =cbrt(Math.abs(x0) * Math.abs(x1) * Math.abs(x2))
+fun strCalculate(x0: String, x1: String): Int {
+    var result = 0
+    var index = 0
+    do {
+        val charX0 = x0[index]
+        val charX1 = x1[index]
+        val doubleDifference = if (charX0 == 'C' && charX1 == 'G' || charX0 == 'G' && charX1 == 'C') 2 else 1
+        if (charX0 != charX1) {
+            result += doubleDifference
+        }
+        index++
+    } while (index < x0.length)
+    return result
+}
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-    val cat3: String="Рудий \uD83D\uDC06"
-    val cat3age: Int=6
-    val cat3weight: Float=8.2f
-    println("Кошеня №3 - $cat3 віком $cat3age з вагою $cat3weight кг")
-    }
+    val iResult = iCalculate(5, 67, 57, -75, 80)
+    val dResult = dCalculate(-25.38, -111.3, -137.76)
+    val strResult = strCalculate("ATGCJ", "TACGJ")
+    println("iCalculate result: $iResult")
+    println("dCalculate result: $dResult")
+    println("strCalculate result: $strResult")
+    startTestUi(seed(), labNumber())
+}
