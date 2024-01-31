b670671c5a2a5ce19c22719c428de263ca64d1d6
ne4eHIOLLIka <152488731+ne4eHIOLLIka@users.noreply.github.com>
2024-01-31 18:43:10 +0200

    lab 2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -14,9 +14,9 @@ fun strCalculate(x0: String, x1: String): Int {
     do {
         val charX0 = x0[index]
         val charX1 = x1[index]
-        val ab1 = if ( charX0== 'C' &&  charX1 == 'G') 2 else 0
+        val ab1 = if (( charX0== 'C' &&  charX1 == 'G')||( charX0== 'G' &&  charX1 == 'C')) 2 else 0
         val ab2 = if ( charX0== 'C' &&  charX1 != 'C' &&  charX1 != 'G') 1 else 0
-        val ab3=if ( charX0== 'G' &&  charX1 != 'G') 1 else 0
+        val ab3=if ( charX0== 'G' &&  charX1 != 'G'&& charX1 != 'C') 1 else 0
         val ab4=if ( charX1== 'G' &&  charX0 != 'G'&& charX0 != 'C') 1 else 0
         val ab5=if ( charX1== 'C' &&  charX0 != 'C'&&  charX0 != 'G') 1 else 0
         result += ab1+ab2+ab3+ab4+ab5
