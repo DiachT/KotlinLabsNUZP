fffceb32384cedecfbd111b371a101c5e2ec86e3
ne4eHIOLLIka <152488731+ne4eHIOLLIka@users.noreply.github.com>
2024-01-31 16:00:06 +0200

    lab 2 variant 41

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,6 +1,5 @@
 import com.diacht.ktest.compose.startTestUi
 import me.alexander.helloworld.BuildConfig
-import kotlin.math.min
 import kotlin.math.sin
 import kotlin.math.cbrt
 fun seed(): String = "ne4eHIOLLIka"
@@ -8,7 +7,7 @@ fun labNumber(): Int = BuildConfig.LAB_NUMBER
 fun iCalculate(x0: Int=5, x1: Int=67, x2: Int=57, x3: Int=-75, x4: Int=80): Double {
     val a=minOf(x0, x1, x2, x3, x4)
     return sin(a.toDouble())}
-fun dCalculate(x0: Double=-25.38, x1: Double=-111.3, x2: Double=-137.76): Double =cbrt(Math.abs(x0) * Math.abs(x1) * Math.abs(x2))
+fun dCalculate(x0: Double=-25.38, x1: Double=-111.3, x2: Double=-137.76): Double =cbrt(Math.abs(x0) + Math.abs(x1) + Math.abs(x2))
 fun strCalculate(x0: String, x1: String): Int {
     var result = 0
     var index = 0
