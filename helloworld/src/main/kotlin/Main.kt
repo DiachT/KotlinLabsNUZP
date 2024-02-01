99d2248c8124ced4994d2fd220695ad73d96ac3e
ne4eHIOLLIka <152488731+ne4eHIOLLIka@users.noreply.github.com>
2024-02-01 14:44:02 +0200

    lab 4 variant 41

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,33 +1,38 @@
 import com.diacht.ktest.compose.startTestUi
 import me.alexander.helloworld.BuildConfig
-import kotlin.math.sin
-import kotlin.math.cbrt
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.URL
+import kotlinx.coroutines.*
+import  kotlin.math.ln
 fun seed(): String = "ne4eHIOLLIka"
-fun labNumber(): Int = BuildConfig.LAB_NUMBER
-fun iCalculate(x0: Int=5, x1: Int=67, x2: Int=57, x3: Int=-75, x4: Int=80): Double {
-    val a=minOf(x0, x1, x2, x3, x4)
-    return sin(a.toDouble())}
-fun dCalculate(x0: Double=-25.38, x1: Double=-111.3, x2: Double=-137.76): Double =cbrt(Math.abs(x0) + Math.abs(x1) + Math.abs(x2))
-fun strCalculate(x0: String, x1: String): Int {
-    var result = 0
-    var index = 0
-    do {
-        val charX0 = x0[index]
-        val charX1 = x1[index]
-        val ab1 = if (( charX0== 'C' &&  charX1 == 'G')||( charX0== 'G' &&  charX1 == 'C')) 1 else 0
-        val ab2 = if ( charX0!=charX1) 1 else 0
-        result += ab1+ab2
-        index++
-    } while (index < x0.length)
-    return result
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
+suspend fun getNumberFromServer(string: String): Int {
+    return withContext(Dispatchers.IO) {
+        val url = URL("http://diacht.2vsoft.com/api/send-number?message=$string")
+        val connection = url.openConnection()
+        connection.connect()
+        val input = connection.getInputStream()
+        val buffer = ByteArray(128)
+        val bytesRead = input.read(buffer)
+        input.close()
+        String(buffer, 0, bytesRead).toInt()
+    }
 }
-fun main(args: Array<String>) {
+suspend fun serverDataCalculate(strList: List<String>) : Double = coroutineScope {
+    val defferendList = strList.map { async { getNumberFromServer(it) } }
+    val res = defferendList.awaitAll()
+    //println("Numbers: $res")
+    val pos=res.filter { it>=0 }.sumByDouble { it.toDouble() }
+    val neg=res.filter { it<0 }.sumByDouble { it.toDouble() }
+    //val sum = res.map{ it.toDouble()}.reduce { acc, next -> acc + next }
+    ln(pos-neg)
+}
+fun main() = runBlocking {
+    val result=serverDataCalculate(listOf("x0","x1","x2","x3","x4","x5"))
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    val iResult = iCalculate()
-    val dResult = dCalculate()
-    val strResult = strCalculate("GGGCJTTCJT", "GGCAJTGCJT")
-    println("iCalculate result: $iResult")
-    println("dCalculate result: $dResult")
-    println("strCalculate result: $strResult")
+    println("Resurt: $result")
     //startTestUi(seed(), labNumber())
 }
+
+
