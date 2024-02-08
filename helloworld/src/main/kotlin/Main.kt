47344c146cb0f1315b63ac5450b36d67af717d40
Ultrahihihaha <zelikkostey@gmail.com>
2024-02-08 21:34:31 +0200

    lab 1

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,21 +1,21 @@
-fun seed():String="Зелік"
+fun seed():String="ZelikK "
 
-fun labNumber(): Int = 1
+fun labNumber(): Int=1
 
 fun main(args:Array<String>){
-    println("Лабораторна робота №${labNumber()} користувача ${seed()}")
+    println("Лабораторна робота № ${labNumber()} користувача ${seed()}")
 
-    var kitty="Броша"
-    kitty +="\uD83D\uDC31"
-    val age = 15
+    var kitty="Васько"
+    kitty+="\uD83D\uDC31"
+    val age=7
     println("Кошеня №1 - $kitty віком $age років")
 
-    val catName: String="Кіт \uD83D\uDC31"
-    val weight:Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
+    val catName:String="Мурзик \uD83D\uDC08"
+    val weight:Float=3.5f
+    println("Кошеня №2 -$catName з вагою $weight кг")
 
-    val cat3: String="Рудий \uD83D\uDC06"
-    val age3=6
-    val weight3:Float = 8.2f
-    println("Кошеня №3 - $cat3 віком $age3 років, з вагою $weight кг")
+    val catName2:String="Рудий \uD83D\uDC06"
+    val age2 = 6
+    val weight2:Float=8.2f
+    println("Кошеня №3 -$catName2 віком $age2 років, вагою $weight2 кг")
 }
